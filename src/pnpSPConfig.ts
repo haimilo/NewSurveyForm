aab29cb84f10c6c1e5a0503d57aaf0185b124fda
Harry <nthai7@cmcglobal.vn>
2023-03-28 09:55:31 +0700

    Update and config pnp/sp package

---
+++ src/pnpSPConfig.ts
@@ -0,0 +1,17 @@
+import { WebPartContext } from "@microsoft/sp-webpart-base";
+import { LogLevel, PnPLogging } from "@pnp/logging";
+import { spfi, SPFI, SPFx } from "@pnp/sp";
+
+import "@pnp/sp/webs";
+import "@pnp/sp/lists";
+import "@pnp/sp/items";
+import "@pnp/sp/batching";
+
+export function getSP(context?: WebPartContext): SPFI {
+    let _sp: SPFI = null;
+    if (_sp === null && context !== null) {
+        _sp = spfi().using(SPFx(context)).using(PnPLogging(LogLevel.Info));
+    }
+
+    return _sp;
+}
